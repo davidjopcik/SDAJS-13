ab204695890f3d46733e20f719a199782a480a3e
Dávid Jopčík <david.jopcik@gmail.com>
2022-01-22 12:29:37 +0100

    oprava

--- jopcikdavid/Task_02/button.js
+++ jopcikdavid/Task_02/button.js
@@ -1,22 +1,23 @@
-const clickButton = document.getElementById("buttonClick")
-const loading = document.querySelector(".circle")
+const clickButton = document.getElementById("buttonClick");
+const loading = document.querySelector(".circle");
 
 clickButton.addEventListener("click", function spusti() {
     const originalText = clickButton.innerText;
     clickButton.innerText = "Loading...";
 
-    loading.classList.add("progress")
-    loading.classList.add("loading")
-    clickButton.classList.add("loadingButton")
+    loading.classList.add("progress");
+    loading.classList.add("loading");
+    clickButton.classList.add("loadingButton");
 
     // <button disabled>Tlačítko</button>
     clickButton.disabled = true;
 
     setTimeout(() => {
         clickButton.innerText = originalText;
-        loading.classList.remove("progress")
-        loading.classList.remove("loading")
-        clickButton.classList.remove("loadingButton")
+        loading.classList.remove("progress");
+        loading.classList.remove("loading");
+        clickButton.classList.remove("loadingButton");
+
         clickButton.disabled = false;
     }, 2000);
-})
+});
