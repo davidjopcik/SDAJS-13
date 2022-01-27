9c03175e022d4eed6a9f211a6e15526b13bf42d6
petrpida <petstu@email.cz>
2022-01-27 19:09:15 +0100

    splashScreen finished

--- petrpida/loadingScreen/script.js
+++ petrpida/loadingScreen/script.js
@@ -1,24 +1,8 @@
 const loadingScreen = document.querySelector('.loading');
-console.log(loadingScreen)
 const firstPage = document.querySelector('.container');
-console.log(firstPage)
-
-firstPage.classList.add('loading');
-
-/* loadingScreen.addEventListener('loadeddata', async function () {
-  console.log('loading data');
-  
-  firstPage.classList.add('loading');
-  loadingScreen.classList.add('loading');
-
-  await timeout (5000)
-
-  firstPage.classList.remove('loading');
-  loadingScreen.classList.remove('loading');
-
-}); */
 
 firstPage.addEventListener('animationend', () => {
     firstPage.classList.remove('loading')
-  });
-  
+    loadingScreen.remove();
+});
+ 
